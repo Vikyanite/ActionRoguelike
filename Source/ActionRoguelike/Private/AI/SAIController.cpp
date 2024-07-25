f22f7ab39bc9ee7b848b0ffc63e17de3c8de59c5
Vic <324165877@qq.com>
2024-07-25 16:19:57 +0800

    Lesson40[Lecture10]Bot Behaviors and Movement

---
+++ Source/ActionRoguelike/Private/AI/SAIController.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SAIController.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+// Sets default values
+ASAIController::ASAIController()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ASAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	RunBehaviorTree(BehaviorTree);
+	
+	if (APawn* MyPawn = UGameplayStatics::GetPlayerPawn(this, 0))
+	{
+		GetBlackboardComponent()->SetValueAsVector("MoveToLocation", MyPawn->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsObject("TargetActor", MyPawn);
+	}
+}
+
+// Called every frame
+void ASAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/ActionRoguelike/Public/AI/SAIController.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "SAIController.generated.h"
+
+UCLASS()
+class ACTIONROGUELIKE_API ASAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ASAIController();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, Category="AI")
+	class UBehaviorTree* BehaviorTree;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
