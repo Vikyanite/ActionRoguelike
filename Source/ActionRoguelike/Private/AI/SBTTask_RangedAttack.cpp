5a4282d2c65dfbad8347bff8c3129fa11911d7d5
Vic <324165877@qq.com>
2024-07-26 20:53:16 +0800

    Lesson42[Lecture11]Custom C++ BT Task for Ranged Attacks

---
+++ Source/ActionRoguelike/Private/AI/SBTTask_RangedAttack.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SBTTask_RangedAttack.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Character.h"
+
+EBTNodeResult::Type USBTTask_RangedAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	if (!BlackboardComp)
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	AActor* TargetActor = Cast<AActor>(BlackboardComp->GetValueAsObject("TargetActor"));
+	if (!TargetActor)
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	if (!AIController)
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	ACharacter* AICharacter = Cast<ACharacter>(AIController->GetPawn());
+	if (!AICharacter)
+	{
+		return EBTNodeResult::Failed;
+	}
+	FVector MuzzleLocation = AICharacter->GetMesh()->GetSocketLocation("Muzzle_01");
+	FRotator MuzzleRotation = (TargetActor->GetActorLocation() - MuzzleLocation).Rotation();
+
+	FActorSpawnParameters Params;
+	Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+	AActor* NewProj = GetWorld()->SpawnActor<AActor>(ProjectileClass, MuzzleLocation, MuzzleRotation, Params); 
+
+	return NewProj ? EBTNodeResult::Succeeded : EBTNodeResult::Failed;
+}
