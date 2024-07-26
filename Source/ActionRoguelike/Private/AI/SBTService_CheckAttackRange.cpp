b5499f091624c50f12544c4a2b6146656c471137
Vic <324165877@qq.com>
2024-07-26 15:56:15 +0800

    Lesson41[Lecture10]Custom Check Attack Range Service for BT

---
+++ Source/ActionRoguelike/Private/AI/SBTService_CheckAttackRange.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SBTService_CheckAttackRange.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+void USBTService_CheckAttackRange::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	// check the distance between the AI pawn and target actor
+	if (UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent())
+	{
+		if (AActor* TargetActor = Cast<AActor>(BlackboardComp->GetValueAsObject("TargetActor")))
+		{
+			if (AAIController* AIController = OwnerComp.GetAIOwner())
+			{
+				if (AActor* AIPawn = AIController->GetPawn())
+				{
+					float DistanceTo = FVector::Dist(AIPawn->GetActorLocation(), TargetActor->GetActorLocation());
+					bool bWithinRange = DistanceTo <= 2000.f;
+					bool bHasLOS = false;
+					
+					if (bWithinRange)
+					{
+						bHasLOS = AIController->LineOfSightTo(TargetActor);
+					}
+					
+					BlackboardComp->SetValueAsBool(AttackRangerKey.SelectedKeyName, bWithinRange && bHasLOS);
+				}
+			}
+		}
+	}
+}
