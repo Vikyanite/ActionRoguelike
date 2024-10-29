28b13af2211e755c1b6b83a9a42a78bb477ea499
Vic <324165877@qq.com>
2024-10-29 14:45:09 +0800

    Lesson103[Lecture26]Animation State Machines (Stunned Effect) & C++ AnimInstance

---
+++ Source/ActionRoguelike/Private/SAnimInstance.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SAnimInstance.h"
+
+#include "GameplayTagContainer.h"
+#include "SActionComponent.h"
+
+void USAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	if (AActor* OwningActor = GetOwningActor())
+	{
+		ActionComp = OwningActor->FindComponentByClass<USActionComponent>();
+	}
+}
+
+void USAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	static FGameplayTag StunnedTag = FGameplayTag::RequestGameplayTag("Status.Stunned");
+	if (ActionComp)
+	{
+		bIsStunned = ActionComp->ActiveGameplayTags.HasTag(StunnedTag);
+	}
+}
